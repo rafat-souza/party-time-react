c81a5cb61bd8544eeb6b756284e8cabf951b5183
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-14 18:23:00 -0300

    Home styles

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import partyFetch from "../axios/config";
 
+import "./Home.css";
+
 const Home = () => {
   const [parties, setParties] = useState(null);
 
