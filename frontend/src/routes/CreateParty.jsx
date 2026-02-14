52c35abd1e5e7a9336b3b323955571a64c533753
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-14 19:29:02 -0300

    Jsx da página de criar festas e serviços adicionados

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const CreateParty = () => {
-  return <div>CreateParty</div>;
+  return (
+    <div className="form-page">
+      <h2>Crie sua próxima festa</h2>
+      <p>Defina o seu orçamento e escolha os serviços</p>
+      <form>
+        <label>
+          <span>Nome da festa:</span>
+          <input type="text" placeholder="Seja criativo..." required />
+        </label>
+        <label>
+          <span>Anfitrião:</span>
+          <input type="text" placeholder="Quem está dando a festa?" required />
+        </label>
+        <label>
+          <span>Descrição:</span>
+          <textarea placeholder="Conte mais sobre a festa" required></textarea>
+        </label>
+        <label>
+          <span>Orçamento:</span>
+          <input
+            type="number"
+            placeholder="Quando você pretende investir?"
+            required
+          />
+        </label>
+        <label>
+          <span>Imagem de divulgação:</span>
+          <input type="text" placeholder="Insira a URL da imagem" required />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            <p>serviços...</p>
+          </div>
+        </div>
+        <input type="submit" value="Criar festa" className="btn" />
+      </form>
+    </div>
+  );
 };
 
 export default CreateParty;
