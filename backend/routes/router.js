5bfdd3c26d28f48cb2428363f5f63d7e8950dd1a
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-13 23:25:30 -0300

    Rotas das festas

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,4 +5,8 @@ const servicesRouter = require("./services");
 
 router.use("/", servicesRouter);
 
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
+
 module.exports = router;
