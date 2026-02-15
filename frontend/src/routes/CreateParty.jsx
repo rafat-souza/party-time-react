9249e39526df064b560437372c33b342ead2258c
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-15 00:20:05 -0300

    Importando serviços pro front-end

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import partyFetch from "../axios/config";
 
 const CreateParty = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const loadServices = async () => {
+      const res = await partyFetch.get("/services");
+
+      setServices(res.data);
+    };
+
+    loadServices();
+  }, []);
+
   return (
     <div className="form-page">
       <h2>Crie sua próxima festa</h2>
@@ -33,7 +48,18 @@ const CreateParty = () => {
         <div>
           <h2>Escolha os serviços</h2>
           <div className="services-container">
-            <p>serviços...</p>
+            {services.length === 0 && <p>Carregando...</p>}
+            {services.length > 0 &&
+              services.map((service) => (
+                <div className="service" key={service._id}>
+                  <img src={service.image} alt={service.name} />
+                  <p className="service-name">{service.name}</p>
+                  <p className="service-price">R${service.price}</p>
+                  <div className="checkbox-container">
+                    <input type="checkbox" value={service._id} />
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
         <input type="submit" value="Criar festa" className="btn" />
