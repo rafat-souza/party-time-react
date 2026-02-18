a9991c2c9207abed714a9a988d9da47a2091e61e
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-18 12:59:16 -0300

    Sobre a senha sem .env

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 async function main() {
   try {
     await mongoose.connect(
+      // A senha do usuário foi trocada antes de expor o repositório ao público
       "mongodb+srv://rafael2:ZPHIogskbibvxAJy@cluster0.nwsqhwn.mongodb.net/?appName=Cluster0",
     );
 
