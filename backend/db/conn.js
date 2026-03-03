37249cd2b5706d76231975ddbfc0fc083c58e36d
Rafael Toledo <ch1at@outlook.com>
2026-03-03 13:03:54 -0300

    update

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 async function main() {
   try {
     await mongoose.connect(
-      // A senha do usuário que estava nos commits antigos foi trocada antes de expor o repositório ao público.
       `mongodb+srv://rafael2:${process.env.SENHA_DB}@cluster0.nwsqhwn.mongodb.net/?appName=Cluster0`,
     );
 
