1d8a510b0fa76b9fcb04d028c68cb21812996c8b
Rafael Toledo Souza <ch1at@outlook.com>
2026-03-03 12:26:02 -0300

    update

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,7 +4,7 @@ async function main() {
   try {
     await mongoose.connect(
       // A senha do usuário foi trocada antes de expor o repositório ao público
-      "mongodb+srv://rafael2:ZPHIogskbibvxAJy@cluster0.nwsqhwn.mongodb.net/?appName=Cluster0",
+      `mongodb+srv://rafael2:${process.env.SENHA_DB}@cluster0.nwsqhwn.mongodb.net/?appName=Cluster0`,
     );
 
     console.log("Conectado ao banco");
